9749394a57f043758b59b8070a9e9cea63b9324e
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-05 15:46:40 -0400

    Save card details instead of card number

--- lib/transbank/sdk/webpay/webpay_plus/responses/transaction_commit_response.rb
+++ lib/transbank/sdk/webpay/webpay_plus/responses/transaction_commit_response.rb
@@ -12,9 +12,7 @@ module Transbank
         attr_accessor *FIELDS
 
         def initialize(json)
-          fields = FIELDS - [:card_number]
           fields.each {|field| send("#{field}=", json["#{field}"]) }
-          self.card_number = json['card_details']['card_number']
         end
       end
     end
