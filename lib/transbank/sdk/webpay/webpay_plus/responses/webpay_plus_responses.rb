29b6b2225ce9e284c821e39ad8415136997d47bf
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-05 10:55:36 -0400

    Fix require paths

--- lib/transbank/sdk/webpay/webpay_plus/responses/webpay_plus_responses.rb
+++ lib/transbank/sdk/webpay/webpay_plus/responses/webpay_plus_responses.rb
@@ -1,2 +1,2 @@
-require 'transbank/sdk/onepay/responses/transaction_create_response'
-require 'transbank/sdk/onepay/responses/transaction_commit_response'
+require 'transbank/sdk/webpay/webpay_plus/responses/transaction_create_response'
+require 'transbank/sdk/webpay/webpay_plus/responses/transaction_commit_response'
