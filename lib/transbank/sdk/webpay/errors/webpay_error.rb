5201466cba27b28c44383ff80d9bab6bad39f1f2
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-05 09:31:40 -0400

    Fix errors to use message, code instead of a hash
    - Fix errors using a hash {message, code}, and instead it now uses two
    params message, code, for compat with normal ruby exceptions

--- lib/transbank/sdk/webpay/errors/webpay_error.rb
+++ lib/transbank/sdk/webpay/errors/webpay_error.rb
@@ -3,9 +3,10 @@ module Transbank
     module Errors
       class WebpayError < StandardError
         attr_accessor :message, :code
-        def initialize(message:, code:)
+        def initialize(message, code)
           self.message = message
           self.code = code
+          super(message)
         end
       end
     end
--- lib/transbank/sdk/webpay/webpay_plus/errors/transaction_create_error.rb
+++ lib/transbank/sdk/webpay/webpay_plus/errors/transaction_create_error.rb
@@ -3,6 +3,11 @@ module Transbank
     module WebpayPlus
       module Errors
         class TransactionCreateError < ::Transbank::Webpay::Errors::WebpayError
+          attr_reader :code
+          def initialize(message, code)
+            @code = code
+            super(message, code)
+          end
         end
       end
     end
