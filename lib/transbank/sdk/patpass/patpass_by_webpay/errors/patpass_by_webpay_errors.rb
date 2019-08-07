f5050b55ab3f33eb8c81a0779ca4d8495f4f626d
Yrvin Escorihuela <yher136@gmail.com>
2019-08-07 18:30:29 -0400

    Replace errors files

--- lib/transbank/sdk/patpass/patpass_by_webpay/errors/patpass_by_webpay_errors.rb
+++ lib/transbank/sdk/patpass/patpass_by_webpay/errors/patpass_by_webpay_errors.rb
@@ -2,6 +2,7 @@ require 'transbank/sdk/patpass/patpass_by_webpay/errors/transaction_create_error
 require 'transbank/sdk/patpass/patpass_by_webpay/errors/transaction_commit_error'
 require 'transbank/sdk/patpass/patpass_by_webpay/errors/transaction_status_error'
 
+require 'transbank/sdk/patpass/patpass_by_webpay/errors/integration_type_error'
 
 module Transbank
   module Patpass
