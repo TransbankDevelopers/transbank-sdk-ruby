a0c3e53d8e5fb508f459bf7ac83846e1b60e06ea
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-07 10:44:38 -0400

    Add Options, Refactor options logic, add upcase
    - Add the missing Options class
    - Refactor the Options usage logic in Transaction to use the safe
    navigation operator
    - Upcase the given param on integration_type setter &
    integration_type_url getter

--- lib/transbank/sdk/webpay/webpay_plus/transaction.rb
+++ lib/transbank/sdk/webpay/webpay_plus/transaction.rb
@@ -93,15 +93,11 @@ module Transbank
           end
 
           def refund(token:, amount:, options:nil)
-            if options.nil?
-              api_key = default_integration_params[:api_key]
-              commerce_code = default_integration_params[:commerce_code]
-              base_url = default_integration_params[:base_url]
-            else
-              api_key = options.api_key || default_integration_params[:api_key]
-              commerce_code = options.commerce_code || default_integration_params[:api_key]
-              base_url = WebpayPlus::Base.integration_types[options.integration_type] || default_integration_params[:base_url]
-            end
+            api_key = options&.api_key || default_integration_params[:api_key]
+            commerce_code = options&.commerce_code || default_integration_params[:commerce_code]
+            integration_type = options&.integration_type || default_integration_params[:integration_type]
+            base_url = integration_type.nil? ? WebpayPlus::Base::integration_type[:TEST] : WebpayPlus::Base.integration_type_url(integration_type)
+
             url = base_url + REFUND_TRANSACTION_ENDPOINT.gsub(':token', token)
             headers = webpay_headers(commerce_code: commerce_code, api_key: api_key)
             body = {amount: amount}
@@ -112,15 +108,11 @@ module Transbank
           end
 
           def status(token:, options: nil)
-            if options.nil?
-              api_key = default_integration_params[:api_key]
-              commerce_code = default_integration_params[:commerce_code]
-              base_url = default_integration_params[:base_url]
-            else
-              api_key = options.api_key || default_integration_params[:api_key]
-              commerce_code = options.commerce_code || default_integration_params[:api_key]
-              base_url = WebpayPlus::Base.integration_types[options.integration_type] || default_integration_params[:base_url]
-            end
+            api_key = options&.api_key || default_integration_params[:api_key]
+            commerce_code = options&.commerce_code || default_integration_params[:commerce_code]
+            integration_type = options&.integration_type || default_integration_params[:integration_type]
+            base_url = integration_type.nil? ? WebpayPlus::Base::integration_type[:TEST] : WebpayPlus::Base.integration_type_url(integration_type)
+
 
             url = base_url + "#{TRANSACTION_STATUS_ENDPOINT}/#{token}"
             headers = webpay_headers(commerce_code: commerce_code, api_key: api_key)
