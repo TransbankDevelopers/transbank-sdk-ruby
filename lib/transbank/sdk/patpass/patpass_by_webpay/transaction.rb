20bbe9c0250197eb8a29d253a1e14d07a7ca1b96
Yrvin Escorihuela <yher136@gmail.com>
2019-08-08 17:11:00 -0400

    Fix typo in integration_types array

--- lib/transbank/sdk/patpass/patpass_by_webpay/transaction.rb
+++ lib/transbank/sdk/patpass/patpass_by_webpay/transaction.rb
@@ -12,20 +12,20 @@ module Transbank
           def create(buy_order:, session_id:, amount:, return_url:, details: ,options: nil)
 
             api_key = options&.api_key || default_integration_params[:api_key]
-            commerce_code = options&.commerce_code || default_integration_params[:api_key]
-            base_url = PatpassByWebpay::Base.integration_types[options&.integration_type] || default_integration_params[:base_url]
+            commerce_code = options&.commerce_code || default_integration_params[:commerce_code]
+            integration_type = options&.integration_type || default_integration_params[:integration_type]
+            base_url = integration_type.nil? ? PatpassByWebpay::Base::integration_types[:TEST] : PatpassByWebpay::Base.integration_type_url(integration_type)
 
             body = {
                 buy_order: buy_order,
                 session_id: session_id,
                 amount: amount,
                 return_url: return_url,
-                wpm_details: details
+                wpm_detail: details
             }
 
             url = base_url + CREATE_TRANSACTION_ENDPOINT
             headers = webpay_headers(commerce_code: commerce_code, api_key: api_key)
-
             resp = http_post(uri_string: url, body: body, headers: headers, camel_case_keys: false)
             body = JSON.parse(resp.body)
 
