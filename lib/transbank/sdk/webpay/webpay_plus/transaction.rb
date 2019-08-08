0235679982f8a776653f4140e5d42ea64318cdd5
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-08 10:17:17 -0400

    Remove duplicated code
    - This code was duplicated because of merges

--- lib/transbank/sdk/webpay/webpay_plus/transaction.rb
+++ lib/transbank/sdk/webpay/webpay_plus/transaction.rb
@@ -64,56 +64,6 @@ module Transbank
             integration_type = options&.integration_type || default_integration_params[:integration_type]
             base_url = integration_type.nil? ? WebpayPlus::Base::integration_type[:TEST] : WebpayPlus::Base.integration_type_url(integration_type)
 
-
-            url = base_url + "#{TRANSACTION_STATUS_ENDPOINT}/#{token}"
-            headers = webpay_headers(commerce_code: commerce_code, api_key: api_key)
-            resp = http_get(uri_string: url, headers: headers)
-            body = JSON.parse(resp.body)
-            return ::Transbank::Webpay::WebpayPlus::TransactionStatusResponse.new(body) if resp.kind_of? Net::HTTPSuccess
-            raise Errors::TransactionStatusError.new(body['error_message'], resp.code)
-          end
-
-          def commit(token:, options: nil)
-            if options.nil?
-              api_key = default_integration_params[:api_key]
-              commerce_code = default_integration_params[:commerce_code]
-              base_url = default_integration_params[:base_url]
-            else
-              api_key = options.api_key || default_integration_params[:api_key]
-              commerce_code = options.commerce_code || default_integration_params[:api_key]
-              base_url = WebpayPlus::Base.integration_types[options.integration_type] || default_integration_params[:base_url]
-            end
-            url = base_url + COMMIT_TRANSACTION_ENDPOINT + "/#{token}"
-            headers = webpay_headers(commerce_code: commerce_code, api_key: api_key)
-
-            resp = http_put(uri_string: url, body: nil, headers: headers)
-            body = JSON.parse(resp.body)
-            return ::Transbank::Webpay::WebpayPlus::TransactionCommitResponse.new(body) if resp.kind_of? Net::HTTPSuccess
-            raise Errors::TransactionCommitError.new(body['error_message'], resp.code)
-          end
-
-          def refund(token:, amount:, options:nil)
-            api_key = options&.api_key || default_integration_params[:api_key]
-            commerce_code = options&.commerce_code || default_integration_params[:commerce_code]
-            integration_type = options&.integration_type || default_integration_params[:integration_type]
-            base_url = integration_type.nil? ? WebpayPlus::Base::integration_type[:TEST] : WebpayPlus::Base.integration_type_url(integration_type)
-
-            url = base_url + REFUND_TRANSACTION_ENDPOINT.gsub(':token', token)
-            headers = webpay_headers(commerce_code: commerce_code, api_key: api_key)
-            body = {amount: amount}
-            resp = http_post(uri_string: url, body: body, headers: headers)
-            body = JSON.parse(resp.body)
-            return ::Transbank::Webpay::WebpayPlus::TransactionRefundResponse.new(body) if resp.kind_of? Net::HTTPSuccess
-            raise Errors::TransactionRefundError.new(body['error_message'], resp.code)
-          end
-
-          def status(token:, options: nil)
-            api_key = options&.api_key || default_integration_params[:api_key]
-            commerce_code = options&.commerce_code || default_integration_params[:commerce_code]
-            integration_type = options&.integration_type || default_integration_params[:integration_type]
-            base_url = integration_type.nil? ? WebpayPlus::Base::integration_type[:TEST] : WebpayPlus::Base.integration_type_url(integration_type)
-
-
             url = base_url + "#{TRANSACTION_STATUS_ENDPOINT}/#{token}"
             headers = webpay_headers(commerce_code: commerce_code, api_key: api_key)
             resp = http_get(uri_string: url, headers: headers)
