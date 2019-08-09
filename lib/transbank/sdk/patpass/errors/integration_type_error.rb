a704d7b59dbb424498d710ce50780e53dbdba28e
Yrvin Escorihuela <yher136@gmail.com>
2019-08-09 12:07:26 -0400

    Restore integration error namespace and ignore .rakeTasks

--- lib/transbank/sdk/patpass/errors/integration_type_error.rb
+++ lib/transbank/sdk/patpass/errors/integration_type_error.rb
@@ -1,14 +1,7 @@
 module Transbank
   module Patpass
-    module PatpassByWebpay
-      module Errors
-        class IntegrationTypeError < ::Transbank::Patpass::Errors::PatpassError
-          attr_reader :code
-          def initialize(message, code)
-            @code = code
-            super(message, code)
-          end
-        end
+    module Errors
+      class IntegrationTypeError < ::Transbank::Errors::TransbankError
       end
     end
   end
