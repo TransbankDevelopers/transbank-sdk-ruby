834b677d009a6091dde27e872f9b57050095492d
Yrvin Escorihuela <yher136@gmail.com>
2019-08-09 10:46:24 -0400

    Add integration error class to errors namespace

--- lib/transbank/sdk/patpass/patpass_by_webpay/errors/patpass_by_webpay_errors.rb
+++ lib/transbank/sdk/patpass/patpass_by_webpay/errors/patpass_by_webpay_errors.rb
@@ -1,6 +1,8 @@
 require 'transbank/sdk/patpass/patpass_by_webpay/errors/transaction_create_error'
 require 'transbank/sdk/patpass/patpass_by_webpay/errors/transaction_commit_error'
 require 'transbank/sdk/patpass/patpass_by_webpay/errors/transaction_status_error'
+require 'transbank/sdk/patpass/errors/integration_type_error'
+
 
 module Transbank
   module Patpass
