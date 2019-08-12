b0de3c36a7fce18bac3e2a53bb187ac18792845b
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-12 17:02:37 -0400

    Remove superflous bracket

--- lib/transbank/sdk/webpay/webpay_plus/mall/mall_transaction.rb
+++ lib/transbank/sdk/webpay/webpay_plus/mall/mall_transaction.rb
@@ -32,7 +32,7 @@ module Transbank
             commerce_code = options&.commerce_code || default_integration_params[:commerce_code]
             integration_type = options&.integration_type || default_integration_params[:integration_type]
             base_url = integration_type.nil? ? WebpayPlus::Base::integration_type[:TEST] : WebpayPlus::Base.integration_type_url(integration_type)
-            url = base_url + "#{}COMMIT_TRANSACTION_ENDPOINT}/#{token}"
+            url = base_url + "#{COMMIT_TRANSACTION_ENDPOINT}/#{token}"
             headers = webpay_headers(commerce_code: commerce_code, api_key: api_key)
             resp = http_put(uri_string: url, body: {}, headers: headers)
             json = JSON.parse(resp.body)
