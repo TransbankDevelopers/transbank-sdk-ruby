12eb4bcfb92cb9157ae35096d5ac5127866e1d64
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-13 08:59:33 -0400

    Fix status endpoint being used

--- lib/transbank/sdk/webpay/webpay_plus/mall/mall_transaction.rb
+++ lib/transbank/sdk/webpay/webpay_plus/mall/mall_transaction.rb
@@ -64,7 +64,7 @@ module Transbank
             commerce_code = options&.commerce_code || default_integration_params[:commerce_code]
             integration_type = options&.integration_type || default_integration_params[:integration_type]
             base_url = integration_type.nil? ? WebpayPlus::Base::integration_type[:TEST] : WebpayPlus::Base.integration_type_url(integration_type)
-            url = base_url + "#{REFUND_TRANSACTION_ENDPOINT}".gsub(':token', token)
+            url = base_url + "#{TRANSACTION_STATUS_ENDPOINT}".gsub(':token', token)
             headers = webpay_headers(commerce_code: commerce_code, api_key: api_key)
 
             resp = http_get(uri_string: url, headers: headers)
