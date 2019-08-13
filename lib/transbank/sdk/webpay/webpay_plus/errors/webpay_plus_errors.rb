511bad77d2e96502b327c4bb161e07634be66ab2
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-13 15:31:31 -0400

    Remove incorrect require'

--- lib/transbank/sdk/webpay/webpay_plus/errors/webpay_plus_errors.rb
+++ lib/transbank/sdk/webpay/webpay_plus/errors/webpay_plus_errors.rb
@@ -2,7 +2,6 @@ require 'transbank/sdk/webpay/webpay_plus/errors/transaction_create_error'
 require 'transbank/sdk/webpay/webpay_plus/errors/transaction_commit_error'
 require 'transbank/sdk/webpay/webpay_plus/errors/transaction_status_error'
 require 'transbank/sdk/webpay/webpay_plus/errors/transaction_refund_error'
-require 'transbank/sdk/webpay/webpay_plus/errors/transaction_capture_error'
 
 module Transbank
   module Webpay
