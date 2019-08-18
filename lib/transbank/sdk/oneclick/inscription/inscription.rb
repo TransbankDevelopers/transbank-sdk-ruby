7561963e364383205b4ff0b48be5b5a5be1f871f
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-18 19:55:23 -0400

    Fix requires

--- lib/transbank/sdk/oneclick/inscription/inscription.rb
+++ lib/transbank/sdk/oneclick/inscription/inscription.rb
@@ -1,5 +1,3 @@
 require 'transbank/sdk/oneclick/errors/oneclick_error'
-require 'transbank/sdk/oneclick/inscription/inscription'
-require 'transbank/sdk/oneclick/mall/base'
-require 'transbank/sdk/oneclick/mall/mall_transaction'
-require 'transbank/sdk/oneclick/mall/mall_inscription'
+require 'transbank/sdk/oneclick/inscription/errors/oneclick_mall_inscription_errors'
+require 'transbank/sdk/oneclick/inscription/responses/oneclick_mall_inscription_responses'
--- lib/transbank/sdk/oneclick/oneclick.rb
+++ lib/transbank/sdk/oneclick/oneclick.rb
@@ -1,2 +1,3 @@
 require 'transbank/sdk/oneclick/errors/oneclick_error'
+require 'transbank/sdk/oneclick/inscription/inscription'
 require 'transbank/sdk/oneclick/mall/oneclick_mall'
