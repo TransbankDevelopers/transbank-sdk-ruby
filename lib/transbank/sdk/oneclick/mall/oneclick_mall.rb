668c7d8593845790dc94ec7465e991d20f014a0d
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-18 19:29:03 -0400

    Add require for MallInscription

--- lib/transbank/sdk/oneclick/mall/oneclick_mall.rb
+++ lib/transbank/sdk/oneclick/mall/oneclick_mall.rb
@@ -1,4 +1,5 @@
 require 'transbank/sdk/oneclick/mall/errors/oneclick_mall_errors'
 require 'transbank/sdk/oneclick/mall/responses/oneclick_mall_responses'
 require 'transbank/sdk/oneclick/mall/base'
+require 'transbank/sdk/oneclick/mall/mall_inscription'
 require 'transbank/sdk/oneclick/mall/mall_transaction'
