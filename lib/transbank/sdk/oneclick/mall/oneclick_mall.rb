4607f69d45291fb8a55ff16e42031d97516895a3
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-18 18:10:24 -0400

    Remove empty require

--- lib/transbank/sdk/oneclick/mall/oneclick_mall.rb
+++ lib/transbank/sdk/oneclick/mall/oneclick_mall.rb
@@ -1,5 +1,3 @@
-require ''
-
 require 'transbank/sdk/oneclick/mall/errors/oneclick_mall_errors'
 require 'transbank/sdk/oneclick/mall/responses/oneclick_mall_responses'
 require 'transbank/sdk/oneclick/mall/base'
