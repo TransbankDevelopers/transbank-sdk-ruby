633889a95c9e6ca00e9de1ee6002aac0f5b316ee
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-18 19:31:53 -0400

    Make all methods class methods

--- lib/transbank/sdk/oneclick/mall/base.rb
+++ lib/transbank/sdk/oneclick/mall/base.rb
@@ -1,47 +1,48 @@
 module Transbank
   module Webpay
     module Oneclick
-      class << self
-        DEFAULT_API_KEY = '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C'.freeze
+      class Base
+        class << self
+          DEFAULT_API_KEY = '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C'.freeze
 
-        DEFAULT_ONECLICK_MALL_COMMERCE_CODE = '597055555541'.freeze
-        DEFAULT_ONECLICK_MALL_CHILD_COMMERCE_CODES = ['597055555542'.freeze,
-                                                      '597055555543'.freeze].freeze
+          DEFAULT_ONECLICK_MALL_COMMERCE_CODE = '597055555541'.freeze
+          DEFAULT_ONECLICK_MALL_CHILD_COMMERCE_CODES = ['597055555542'.freeze,
+                                                        '597055555543'.freeze].freeze
 
-        @api_key = DEFAULT_API_KEY
-        @commerce_code = DEFAULT_ONECLICK_MALL_COMMERCE_CODE
-        @integration_type = :TEST
-        @integration_types = {
-          LIVE: "https://webpay3g.transbank.cl/",
-          TEST: "https://webpay3gint.transbank.cl/"
-        }
-        attr_reader :integration_types
-        attr_accessor :api_key, :integration_type, :commerce_code
-
-        def integration_type_url(integration_type)
-          type = integration_type.upcase.to_sym
-          return @integration_types[type] unless @integration_types[type].nil?
-          valid_values = @integration_types.keys.join(', ')
-          raise Transbank::Webpay::Errors::IntegrationTypeError, "Invalid integration type, valid values are #{valid_values}"
-        end
-
-        def current_integration_type_url
-          @integration_types[@integration_type]
-        end
-
-        def integration_type=(integration_type)
-          type = integration_type.upcase.to_sym
-          return @integration_type = type unless @integration_types[type].nil?
-          valid_values = @integration_types.keys.join(', ')
-          raise Transbank::Webpay::Errors::IntegrationTypeError, "Invalid integration type, valid values are #{valid_values}"
-        end
-
-        def configure_mall_for_testing
           @api_key = DEFAULT_API_KEY
           @commerce_code = DEFAULT_ONECLICK_MALL_COMMERCE_CODE
-          self.integration_type = :TEST
-        end
+          @integration_type = :TEST
+          @integration_types = {
+            LIVE: "https://webpay3g.transbank.cl/",
+            TEST: "https://webpay3gint.transbank.cl/"
+          }
+          attr_reader :integration_types
+          attr_accessor :api_key, :integration_type, :commerce_code
 
+          def integration_type_url(integration_type)
+            type = integration_type.upcase.to_sym
+            return @integration_types[type] unless @integration_types[type].nil?
+            valid_values = @integration_types.keys.join(', ')
+            raise Transbank::Webpay::Errors::IntegrationTypeError, "Invalid integration type, valid values are #{valid_values}"
+          end
+
+          def current_integration_type_url
+            @integration_types[@integration_type]
+          end
+
+          def integration_type=(integration_type)
+            type = integration_type.upcase.to_sym
+            return @integration_type = type unless @integration_types[type].nil?
+            valid_values = @integration_types.keys.join(', ')
+            raise Transbank::Webpay::Errors::IntegrationTypeError, "Invalid integration type, valid values are #{valid_values}"
+          end
+
+          def configure_mall_for_testing
+            @api_key = DEFAULT_API_KEY
+            @commerce_code = DEFAULT_ONECLICK_MALL_COMMERCE_CODE
+            self.integration_type = :TEST
+          end
+        end
       end
     end
   end
