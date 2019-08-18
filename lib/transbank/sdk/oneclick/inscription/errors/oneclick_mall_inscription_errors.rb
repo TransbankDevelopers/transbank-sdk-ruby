3829372c7c252e8711ab05acbfce2aa6024b15f6
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-18 19:57:28 -0400

    Fix directory on inscription requires

--- lib/transbank/sdk/oneclick/inscription/errors/oneclick_mall_inscription_errors.rb
+++ lib/transbank/sdk/oneclick/inscription/errors/oneclick_mall_inscription_errors.rb
@@ -1,3 +1,3 @@
-require 'transbank/sdk/oneclick/mall/inscription/errors/mall_inscription_delete_error'
-require 'transbank/sdk/oneclick/mall/inscription/errors/mall_inscription_start_error'
-require 'transbank/sdk/oneclick/mall/inscription/errors/mall_inscription_finish_error'
+require 'transbank/sdk/oneclick/inscription/errors/mall_inscription_delete_error'
+require 'transbank/sdk/oneclick/inscription/errors/mall_inscription_start_error'
+require 'transbank/sdk/oneclick/inscription/errors/mall_inscription_finish_error'
--- lib/transbank/sdk/oneclick/inscription/responses/oneclick_mall_inscription_responses.rb
+++ lib/transbank/sdk/oneclick/inscription/responses/oneclick_mall_inscription_responses.rb
@@ -1,3 +1,3 @@
-require 'transbank/sdk/oneclick/mall/inscription/responses/mall_inscription_delete_response'
-require 'transbank/sdk/oneclick/mall/inscription/responses/mall_inscription_start_response'
-require 'transbank/sdk/oneclick/mall/inscription/responses/mall_inscription_finish_response'
+require 'transbank/sdk/oneclick/inscription/responses/mall_inscription_delete_response'
+require 'transbank/sdk/oneclick/inscription/responses/mall_inscription_start_response'
+require 'transbank/sdk/oneclick/inscription/responses/mall_inscription_finish_response'
