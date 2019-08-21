578dce02e6a991a65711c5a08d1536b9a8a9126e
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-21 15:50:30 -0400

    Fix wrong require due to git rebases

--- lib/transbank/sdk/patpass/patpass_by_webpay/errors/patpass_by_webpay_errors.rb
+++ lib/transbank/sdk/patpass/patpass_by_webpay/errors/patpass_by_webpay_errors.rb
@@ -2,9 +2,6 @@ require 'transbank/sdk/patpass/patpass_by_webpay/errors/transaction_create_error
 require 'transbank/sdk/patpass/patpass_by_webpay/errors/transaction_commit_error'
 require 'transbank/sdk/patpass/patpass_by_webpay/errors/transaction_status_error'
 
-
-require 'transbank/sdk/patpass/patpass_by_webpay/errors/integration_type_error'
-
 module Transbank
   module Patpass
     module PatpassByWebpay
