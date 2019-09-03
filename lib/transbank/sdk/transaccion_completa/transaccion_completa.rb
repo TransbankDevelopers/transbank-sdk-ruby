cade20263d2bf846de0a269447d5b5dc1d54e227
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-09-03 10:18:02 -0300

    Feat/add transaccion completa mall (#38)
    * First iteration of trx completa mall
    
    * Mark child buy orders & commerce codes
    
    * Fix wrong require due to git rebases
    
    * Change #commit to use details[]
    
    * Change installments method signature
    
    - Now uses "buy_order", "commerce_code" instead of "child_buy_order",
    "child_commerce_code"
    - This is because you can send either the child/parent commerce code/buy
    order
    
    * Add handling multiple installments req at once
    
    * Use 'resp' instead of body to create Response object
    
    * Add body definition to Thread block
    
    * Add checking resp type on each response
    
    * Add pry
    
    * Parse body only if all resp are OK
    
    * Correctly parse body on #installments
    
    * Remove Thread call
    
    - Remove using Thread new for each HTTP call for installments
    - This was done because it's complicated to compat this with how the integrator (our end user)
    will use the gem
    - Now calls are done one after another, but there will be no complications regarding thread creation
    that might fail unexpectedly.
    - We can document that the integrator can use Threads on his/her code to make parallel reqs anyways

--- lib/transbank/sdk/transaccion_completa/transaccion_completa.rb
+++ lib/transbank/sdk/transaccion_completa/transaccion_completa.rb
@@ -3,3 +3,6 @@ require 'transbank/sdk/transaccion_completa/responses/transaccion_completa_respo
 require 'transbank/sdk/transaccion_completa/options'
 require 'transbank/sdk/transaccion_completa/base'
 require 'transbank/sdk/transaccion_completa/transaction'
+
+require 'transbank/sdk/transaccion_completa/mall/responses/mall_transaction_responses'
+require 'transbank/sdk/transaccion_completa/mall/mall_transaction'
