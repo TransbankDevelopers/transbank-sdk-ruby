7e2d6e84a8565fcad02c9e95293fd0524776264e
Alba Cárdenas <alba.cardenas@continuum.cl>
2019-12-23 15:34:06 -0300

    Fix oneclick finish parameters

--- lib/transbank/sdk/oneclick/inscription/responses/mall_inscription_finish_response.rb
+++ lib/transbank/sdk/oneclick/inscription/responses/mall_inscription_finish_response.rb
@@ -3,7 +3,7 @@ module Transbank
     module Oneclick
       class MallInscriptionFinishResponse
         FIELDS = [:response_code, :tbk_user, :authorization_code,
-                  :credit_card_type, :last_four_card_digits]
+                  :card_type, :card_number]
         attr_accessor *FIELDS
         def initialize(json)
           FIELDS.each {|field| send("#{field}=", json["#{field}"]) }
