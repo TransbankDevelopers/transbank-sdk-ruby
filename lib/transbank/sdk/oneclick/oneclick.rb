47ca031684ec111f304d5c24fc79b6ca2679bc56
Felipe Fiebig <felipe.fiebig@continuum.cl>
2020-12-03 15:33:56 -0300

    Add configure for testing methods

--- lib/transbank/sdk/oneclick/oneclick.rb
+++ lib/transbank/sdk/oneclick/oneclick.rb
@@ -1,5 +1,12 @@
+require 'transbank/sdk/oneclick/base'
 require 'transbank/sdk/oneclick/errors/oneclick_error'
 require 'transbank/sdk/oneclick/inscription/inscription'
 require 'transbank/sdk/oneclick/mall/oneclick_mall'
 require 'transbank/sdk/oneclick/mall_deferred/mall_deferred_transaction'
 require 'transbank/sdk/oneclick/mall_deferred/mall_deferred_inscription'
+
+module Transbank
+  module Webpay
+    module Oneclick; end
+  end
+end
