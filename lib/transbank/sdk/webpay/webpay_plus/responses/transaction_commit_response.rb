1ca3d906a765bdfc36946860c5cc8f3148df94cb
Felipe Fiebig <felipe.fiebig@continuum.cl>
2021-03-29 17:50:42 -0300

    Add missing status field on commit/status response

--- lib/transbank/sdk/webpay/webpay_plus/responses/transaction_commit_response.rb
+++ lib/transbank/sdk/webpay/webpay_plus/responses/transaction_commit_response.rb
@@ -4,7 +4,7 @@ module Transbank
       class TransactionCommitResponse
 
         FIELDS =
-          [:vci, :amount, :buy_order, :session_id,
+          [:vci, :amount, :status, :buy_order, :session_id,
            :card_detail, :accounting_date, :transaction_date,
            :authorization_code, :payment_type_code, :response_code,
            :installments_amount, :installments_number, :balance]
