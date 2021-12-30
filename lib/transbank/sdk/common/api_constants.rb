5b072d6e109527b591a937da1060b2e8a142941a
unix <mauricio@MacBook-Pro-de-mauricio.local>
2021-12-30 15:07:10 -0500

    class to module

--- lib/transbank/sdk/common/api_constants.rb
+++ lib/transbank/sdk/common/api_constants.rb
@@ -1,6 +1,6 @@
 module Transbank
   module Common  
-    class ApiConstants
+    module ApiConstants
         WEBPAY_ENDPOINT = 'rswebpaytransaction/api/webpay/v1.2'.freeze
         ONECLICK_ENDPOINT = 'rswebpaytransaction/api/oneclick/v1.2'.freeze
         PATPASS_ENDPOINT = 'restpatpass/v1/services'.freeze
