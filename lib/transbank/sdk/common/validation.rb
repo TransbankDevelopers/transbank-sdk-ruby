4c248d36b68121214f5a930ddd4b27108cc2d07d
mvarlic <mvarlic@gmail.com>
2022-09-15 13:33:07 -0500

    update to api 1.3

--- lib/transbank/sdk/common/validation.rb
+++ lib/transbank/sdk/common/validation.rb
@@ -4,7 +4,7 @@ module Transbank
       class Validation
         
         def self.has_text_with_max_length(value, value_max_length,  value_name)
-          if value.empty? 
+          if value.nil? || value.empty? 
             raise Transbank::Shared::TransbankError, "Transbank Error: %s is empty" % [value_name]
           end
           if value.length() > value_max_length
