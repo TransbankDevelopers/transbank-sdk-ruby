296fb16677590ccfc63ea6a3002ed1c8c2e86499
victor mendoza <victor.mendoza@continuumhq.com>
2024-07-01 18:38:31 -0300

    feat: add timeout in products

--- lib/transbank/sdk/webpay/transaccion_completa/mall_transaction.rb
+++ lib/transbank/sdk/webpay/transaccion_completa/mall_transaction.rb
@@ -11,13 +11,13 @@ module Transbank
         REFUND_ENDPOINT = (RESOURCES_URL + '/transactions/%{token}/refunds').freeze
         CAPTURE_ENDPOINT = (RESOURCES_URL + '/transactions/%{token}/capture').freeze
     
-        def initialize(commerce_code = ::Transbank::Common::IntegrationCommerceCodes::TRANSACCION_COMPLETA_MALL, api_key = ::Transbank::Common::IntegrationApiKeys::WEBPAY, environment = DEFAULT_ENVIRONMENT)
+        def initialize(commerce_code = ::Transbank::Common::IntegrationCommerceCodes::TRANSACCION_COMPLETA_MALL, api_key = ::Transbank::Common::IntegrationApiKeys::WEBPAY, environment = DEFAULT_ENVIRONMENT, timeout = ::Transbank::Common::ApiConstants::REQUEST_TIMEOUT)
           super(commerce_code, api_key, environment)
         end
 
         def create(buy_order, session_id, card_number, card_expiration_date, details, cvv = nil)
           request_service = ::Transbank::Shared::RequestService.new(
-            @environment, CREATE_ENDPOINT, @commerce_code, @api_key
+            @environment, CREATE_ENDPOINT, @commerce_code, @api_key, @timeout
           )
           request_service.post({
                                  buy_order: buy_order, session_id: session_id, card_number: card_number, card_expiration_date: card_expiration_date, details: details, cvv: cvv
@@ -26,7 +26,7 @@ module Transbank
 
         def installments(token, details)
           request_service = ::Transbank::Shared::RequestService.new(
-            @environment, format(INSTALLMENTS_ENDPOINT, token: token), @commerce_code, @api_key
+            @environment, format(INSTALLMENTS_ENDPOINT, token: token), @commerce_code, @api_key, @timeout
           )
           details.map { 
             |detail| 
@@ -36,28 +36,28 @@ module Transbank
     
         def commit(token, details)
           request_service = ::Transbank::Shared::RequestService.new(
-            @environment, format(COMMIT_ENDPOINT, token: token), @commerce_code, @api_key
+            @environment, format(COMMIT_ENDPOINT, token: token), @commerce_code, @api_key, @timeout
           )
           request_service.put({details: details})
         end
 
         def status(token)
           request_service = ::Transbank::Shared::RequestService.new(
-            @environment, format(STATUS_ENDPOINT, token: token), @commerce_code, @api_key
+            @environment, format(STATUS_ENDPOINT, token: token), @commerce_code, @api_key, @timeout
           )
           request_service.get
         end
     
         def refund(token, buy_order, commerce_code_child, amount)
           request_service = ::Transbank::Shared::RequestService.new(
-            @environment, format(REFUND_ENDPOINT, token: token), @commerce_code, @api_key
+            @environment, format(REFUND_ENDPOINT, token: token), @commerce_code, @api_key, @timeout
           )
           request_service.post(buy_order: buy_order, commerce_code: commerce_code_child, amount: amount)
         end     
         
         def capture(token, commerce_code, buy_order, authorization_code, amount)
           request_service = ::Transbank::Shared::RequestService.new(
-            @environment, format(CAPTURE_ENDPOINT, token: token), @commerce_code, @api_key
+            @environment, format(CAPTURE_ENDPOINT, token: token), @commerce_code, @api_key, @timeout
           )
           request_service.put(buy_order: buy_order, commerce_code: commerce_code, authorization_code: authorization_code, capture_amount: amount)
         end
