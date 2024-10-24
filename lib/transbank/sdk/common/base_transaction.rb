8a73dcc1b397f8a9fe237365da0e317fd9c90a34
victor mendoza <victor.mendoza@continuumhq.com>
2024-10-24 14:19:21 -0300

    refactor: change params

--- lib/transbank/sdk/common/base_transaction.rb
+++ lib/transbank/sdk/common/base_transaction.rb
@@ -2,15 +2,15 @@ module Transbank
   module Common  
     class BaseTransaction
       
-      def initialize(commerce_code = ::Transbank::Common::IntegrationCommerceCodes::WEBPAY_PLUS, api_key = ::Transbank::Common::IntegrationApiKeys::WEBPAY, environment = DEFAULT_ENVIRONMENT, timeout)
-          @commerce_code = commerce_code
-          @api_key = api_key
-          unless %i[production integration].include?(environment)
+      def initialize(options)
+          @commerce_code = options.commerce_code
+          @api_key = options.api_key
+          unless %i[production integration].include?(options.environment)
             raise ArgumentError, "Environment must be either 'integration' or 'production'"
           end
     
-          @environment = environment
-          @timeout = timeout
+          @environment = options.environment
+          @timeout = options.timeout
       end
     end
   end
