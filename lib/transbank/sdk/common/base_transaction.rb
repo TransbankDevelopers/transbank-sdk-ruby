e5aae6c59118296401ceb788821db11799713891
victor mendoza <victor.mendoza@continuumhq.com>
2024-10-28 12:31:35 -0300

    feat: add validation in base transaction

--- lib/transbank/sdk/common/base_transaction.rb
+++ lib/transbank/sdk/common/base_transaction.rb
@@ -3,11 +3,18 @@ module Transbank
     class BaseTransaction
       
       def initialize(options)
-          @commerce_code = options.commerce_code
-          @api_key = options.api_key
+          required_methods = [:commerce_code, :api_key, :environment, :timeout]
+          missing_methods = required_methods.reject { |method| options.respond_to?(method) }
+
+          unless missing_methods.empty?
+            raise ArgumentError, "Options object must respond to: #{missing_methods.join(', ')}"
+          end
           unless %i[production integration].include?(options.environment)
             raise ArgumentError, "Environment must be either 'integration' or 'production'"
           end
+          
+          @commerce_code = options.commerce_code
+          @api_key = options.api_key
     
           @environment = options.environment
           @timeout = options.timeout
